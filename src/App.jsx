3a5f32ae370e3169f8286ece099442af88fa98ca
ryanmao725 <ryan.mao.725@gmail.com>
2020-06-02 21:10:14 -0400

    Updated theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, {createContext} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {ThemeProvider} from '@material-ui/core';
+import {ThemeProvider, createMuiTheme, CssBaseline, responsiveFontSizes, Typography} from '@material-ui/core';
 import './App.css';
 
 import Profile from 'Profile.js';
@@ -12,16 +12,40 @@ import Login from 'components/authentication/login/Login';
 const ProfileContext = createContext(null);
 // const survey = new SurveyDAG();
 
+let theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#306DDF',
+        },
+        secondary: {
+            main: '#FEAD18',
+        },
+        error: {
+            main: '#F46D66',
+        },
+        info: {
+            main: '#7064D0',
+        },
+        success: {
+            main: '#47C594',
+        },
+    },
+    typography: {
+        fontFamily: 'Poppins',
+    },
+});
+theme = responsiveFontSizes(theme);
+
 const App = () => {
     // We create a new profile object. It should automatically be populated if the user has already logged in.
     const profile = new Profile();
 
     return (
         <ProfileContext.Provider value={profile}>
-            <ThemeProvider>
+            <ThemeProvider theme={theme}>
                 <Router>
                     <Switch>
-                        <Route exact path="/" render={(props) => <p>This is the landing page!</p> }></Route>
+                        <Route exact path="/" render={(props) => <Typography>This is the landing page!</Typography> }></Route>
                         <Route exact path="/login" render={(props) => <Login/>} />
                     </Switch>
                 </Router>
