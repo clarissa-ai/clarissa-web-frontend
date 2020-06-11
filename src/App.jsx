4459f7f124806890207e0ff90a6a297dbaba8614
ryanmao725 <ryan.mao.725@gmail.com>
2020-06-11 16:03:01 -0400

    Added a redirect router that allows us to define a url mapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, {createContext} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import React, {createContext, useEffect, useState} from 'react';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import {ThemeProvider, createMuiTheme, responsiveFontSizes, Typography} from '@material-ui/core';
 import './App.css';
 
@@ -43,15 +43,46 @@ theme = responsiveFontSizes(theme);
 const App = () => {
     // We create a new profile object. It should automatically be populated if the user has already logged in.
     const profile = new Profile();
-    console.log(process.env.REACT_APP_ENDPOINT_BASE);
+    // State to hold the routes object.
+    const [routes, setRoutes] = useState(null);
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_ENDPOINT_BASE}/api/routes`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((response) => {
+            if (!response.ok) {
+                console.log('Failed to retrive routes.');
+                return;
+            }
+            response.json().then((data) => {
+                const {status, message} = data;
+                if (status === 'success') {
+                    setRoutes(data.routes);
+                    console.log(data.routes);
+                } else {
+                    console.log(message);
+                };
+            });
+        });
+    }, []);
+
+    const redirect = [];
+    if (routes) {
+        Object.keys(routes).forEach((key) => redirect.push(<Redirect key={key} from={key} to={routes[key]} />));
+    }
 
     return (
         <ProfileContext.Provider value={profile}>
             <ThemeProvider theme={theme}>
                 <Router>
                     <Switch>
-                        <Route exact path="/" render={(props) => <Typography>This is the landing page!</Typography> }></Route>
-                        <Route exact path="/login" render={(props) => <Login/>} />
+                        <Route exact path='/' render={(props) => <Typography>This is the landing page!</Typography> }></Route>
+                        <Route exact path='/login' render={(props) => <Login/>} />
+                        {redirect}
+                        <Route path='/survey/' render={(props) => console.log(props.location.pathname.replace(props.match.path, ''))} />
+                        <Route render={() => <Typography>This is the 404 page ¯\_(ツ)_/¯</Typography>} />
                     </Switch>
                 </Router>
             </ThemeProvider>
