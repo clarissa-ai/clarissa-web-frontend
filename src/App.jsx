1fc15eea5158e1b3702beddadffe513b558e9dd1
ryanmao725 <ryan.mao.725@gmail.com>
2020-06-13 13:59:33 -0400

    Found issue regarding flashing 404 page. Do not know how to fix

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,11 @@ const App = () => {
 
     const redirect = [];
     if (routes) {
-        Object.keys(routes).forEach((key) => redirect.push(<Redirect from={key} to={routes[key]} />));
+        Object.keys(routes).forEach((key) => {
+            redirect.push(<Redirect key={key} strict from={key} to={routes[key]} />);
+            console.log(redirect);
+        });
+        console.log(routes);
     };
 
     return (
@@ -79,10 +83,11 @@ const App = () => {
             <ThemeProvider theme={theme}>
                 <Router>
                     <Switch>
+                        {redirect}
                         <Route exact path="/" render={(props) => <Typography>This is the landing page!</Typography> } />
                         <Route exact path="/login" render={(props) => <Login/>} />
-                        {redirect}
-                        <Route render={(props) => <Typography>This is the 404 page.</Typography>} />
+                        <Route path='/survey' render={(props) => <Typography>{props.location.pathname}</Typography>} />
+                        {routes ? <Route render={(props) => <Typography>This is the 404 page.</Typography>} /> : []}
                     </Switch>
                 </Router>
             </ThemeProvider>
