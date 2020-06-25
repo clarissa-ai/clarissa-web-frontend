3a2980b67c00b6e79418138760cfa7d55fed8382
Rahul Narla <rahulnarla@Rahuls-MBP.fios-router.home>
2020-06-25 18:40:16 -0400

    fixed coloring of nav bar

--- src/components/navbar/RenderRoutes.jsx
+++ src/components/navbar/RenderRoutes.jsx
@@ -1,7 +1,7 @@
 import React, {forwardRef} from 'react';
 import PropTypes from 'prop-types';
 import {List, ListItem, Typography, makeStyles, Button} from '@material-ui/core';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
     name: {
@@ -29,7 +29,7 @@ const useStyles = makeStyles((theme) => ({
 
 const ConstructRoutes = forwardRef((props, ref) => (
     <div ref={ref} style={{flexGrow: 1}}>
-        <Link {...props}/>
+        <NavLink {...props}/>
     </div>
 ));
 
