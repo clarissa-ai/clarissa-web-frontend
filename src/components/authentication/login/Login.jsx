e6dc19bc1ea999259e6915345a8146994cca1f30
Mei <meiluwork@gmail.com>
2020-06-28 23:27:10 -0400

    Make sign in form

--- src/components/authentication/login/Login.jsx
+++ src/components/authentication/login/Login.jsx
@@ -1,16 +1,107 @@
 import React from 'react';
-import {Grid, TextField} from '@material-ui/core';
-
+import {Grid, Input, InputAdornment, IconButton, Button, Typography, Link, makeStyles, FormControlLabel, Checkbox} from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import {ProfileContext} from 'App';
+
+const useStyles = makeStyles((theme) => ({
+    field: {
+        width: '20rem',
+        fontSize: '.9rem',
+    },
+    button: {
+        backgroundColor: '#47C594',
+        color: '#fff',
+        padding: '0.5rem',
+        width: '20rem',
+        boxShadow: '0px 4px 10px rgba(16, 156, 241, 0.24)',
+        borderRadius: '4px',
+        textTransform: 'none',
+    },
+    link: {
+        color: '#47C594',
+        fontSize: '1rem',
+        fontWeight: 'bold',
+    },
+    checkbox: {
+        color: '#47C594',
+        fontSize: '1rem',
+    },
+    unchecked: {
+        color: '#000',
+    },
+}));
 
 const Login = () => {
     const profile = ProfileContext;
     console.log(profile);
+    const classes = useStyles();
+    const [values, setValues] = React.useState({
+        showPassword: false,
+        password: '',
+        rememberMe: false,
+    });
+
+    const handleChange = (prop) => (event) => {
+        setValues({...values, [prop]: event.target.value});
+    };
+
+    const handleClickShowPassword = () => {
+        setValues({...values, showPassword: !values.showPassword});
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const handleCheckbox = (event) => {
+        setValues({...values, rememberMe: !values.rememberMe});
+    };
 
     return (
-        <Grid container direction="column" alignItems="center" justify="center">
-            <Grid item xs={6}>
-                <TextField label="Email" placeholder="your@email.com" type="email" autoComplete="email" variant="outlined" required margin="dense" fullWidth/> {/* Email */}
+        <Grid container direction="column" alignItems="flex-start" justify='center' spacing={5}>
+            <Grid item><Typography variant='h4' style={{fontWeight: 'bold', color: '#334D6E'}}>Sign In</Typography></Grid>
+            <Grid item><Input id="standard-basic" placeholder="Email" className={classes.field}/></Grid>
+            <Grid item>
+                <Input
+                    className={classes.field}
+                    placeholder="Password"
+                    id="standard-password"
+                    type={values.showPassword ? 'text' : 'password'}
+                    value={values.password}
+                    onChange={handleChange('password')}
+                    endAdornment={
+                        <InputAdornment position="end" color='secondary'>
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                            >
+                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                />
+            </Grid>
+            <Grid item>
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            classes={{root: classes.checkbox, indeterminate: classes.unchecked}}
+                            checked={values.rememberMe}
+                            onChange={handleCheckbox}
+                            color='inherit'
+                        />
+                    }
+                    label="Keep me logged in"
+                />
+            </Grid>
+            <Grid item>
+                <Grid container justify='flex-end' direction='column' spacing={3}>
+                    <Grid item><Button className={classes.button}>Sign In</Button></Grid>
+                    <Grid item><Typography><Link className={classes.link}>Forgot Password?</Link></Typography></Grid>
+                    <Grid item><Typography>Don&apos;t have an account? <Link className={classes.link}>Sign Up</Link></Typography></Grid>
+                </Grid>
             </Grid>
         </Grid>
     );
