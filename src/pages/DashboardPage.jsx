d15122b5e6d7deb99cee8f48e3602e26f93a823b
ryanmao725 <ryan.mao.725@gmail.com>
2020-07-09 17:40:30 -0400

    Protected dashboard page

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -9,6 +9,7 @@ import CompletedSurveyCard from 'components/dashboard/CompletedSurveyCard';
 import TakeSurveyCard from 'components/dashboard/TakeSurveyCard';
 import TopBar from 'components/dashboard/TopBar';
 import InfoCard from 'components/dashboard/InfoCard';
+import {Redirect} from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
     button: {
@@ -39,75 +40,79 @@ const DashboardPage = (props) => {
     const profile = useSelector(profileSelector);
 
     useEffect(()=> {
+        if (!profile.authenticated) return; 
         fetch(`${apiLink}/api/dashboard/get_dashboard`, {
-        credentials: 'include',
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },})
-        .then(res => res.json())
-        .then(res => {
-                setDash(res);
-                setActiveSurveys(res.active_surveys);
-                setCompletedSurveys(res.completed_surveys);
-                setrecentIllness(res.recent_illnesses);
-        },
-        (error) => {
-                console.log(error);
-        },);
+            credentials: 'include',
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },})
+            .then(res => res.json())
+            .then(res => {
+                    setDash(res);
+                    setActiveSurveys(res.active_surveys);
+                    setCompletedSurveys(res.completed_surveys);
+                    setrecentIllness(res.recent_illnesses);
+            },
+            (error) => {
+                    console.log(error);
+            },);
 
-        const userInfo = profile.userInfo;
-        setName(userInfo.first_name);
-    }, [apiLink, profile.userInfo])
+            const userInfo = profile.userInfo;
+            setName(userInfo.first_name);
+    }, [apiLink, profile])
     
+    console.log(profile.authenticated);
+    return (
+        !profile.authenticated ? <Redirect to='/login' /> :
+            <Fade in timeout={1000}>
+                <div className={classes.container}>
+                    <Grid container direction='row' spacing={0} justify='center' alignItems='stretch' alignContent='stretch'>
+                        <Grid item><TopBar><Button color='primary' variant="contained" style={{textTransform: 'none'}}>New Illness</Button></TopBar></Grid>
+                        <Grid item><ResponsiveDrawer/></Grid>
 
-    return <Fade in timeout={1000}>
-    <div className={classes.container}>
-    <Grid container direction='row' spacing={0} justify='center' alignItems='stretch' alignContent='stretch'>
-        <Grid item><TopBar><Button color='primary' variant="contained" style={{textTransform: 'none'}}>New Illness</Button></TopBar></Grid>
-        <Grid item><ResponsiveDrawer/></Grid>
+                        <Grid item xs={12} md={6} xl={7} style={{marginLeft: '1rem'}}>
+                            <Grid container direction='column' spacing={2}>
+                                <Grid item>
+                                    <div className={classes.greetingsContainer}>
+                                        <Typography variant='h6'><Box fontWeight='bold'>Hey {userName}!</Box></Typography>
+                                        <Typography variant='subtitle2' style={{opacity: '0.7'}}>View and manage your important information here.</Typography>
+                                    </div>
+                                </Grid>
 
-        <Grid item xs={12} md={6} xl={7} style={{marginLeft: '1rem'}}>
-            <Grid container direction='column' spacing={2}>
-                <Grid item>
-                    <div className={classes.greetingsContainer}>
-                        <Typography variant='h6'><Box fontWeight='bold'>Hey {userName}!</Box></Typography>
-                        <Typography variant='subtitle2' style={{opacity: '0.7'}}>View and manage your important information here.</Typography>
-                    </div>
-                </Grid>
+                                <Grid item>
+                                    <RecentIllness >
+                                        {recentIllness.map((illness, index) => {
+                                            return <Grid item><InfoCard key={index} title='' date={`${illness.created_on} - ${illness.updated_on}`} status={illness.active} symptomcount={illness.symptom_count} /></Grid>
+                                        })}
+                                    </RecentIllness>
+                                </Grid>
+                            </Grid>
+                        </Grid>
 
-                <Grid item>
-                    <RecentIllness >
-                        {recentIllness.map((illness, index) => {
-                            return <Grid item><InfoCard key={index} title='' date={`${illness.created_on} - ${illness.updated_on}`} status={illness.active} symptomcount={illness.symptom_count} /></Grid>
-                        })}
-                    </RecentIllness>
-                </Grid>
-            </Grid>
-        </Grid>
+                        <Grid item xs={12} md={3} xl={3}style={{marginLeft: '1rem'}}>
+                            <Grid container direction='column' spacing={2}>
+                                <Grid item><StatsCard illnesscount={dashData.illness_count} symptomcount={dashData.symptom_count} visitcount={dashData.response_count}/></Grid>
+                                <Grid item>
+                                    <CompletedSurveyCard>
+                                        {completedSurveys.map((completed_surveys, index) => {
+                                        return <Grid item><InfoCard key={index} title={completed_surveys.title} status={null} link={completed_surveys.id}/></Grid>
+                                    })}
+                                    </CompletedSurveyCard>
+                                </Grid>
 
-        <Grid item xs={12} md={3} xl={3}style={{marginLeft: '1rem'}}>
-            <Grid container direction='column' spacing={2}>
-                <Grid item><StatsCard illnesscount={dashData.illness_count} symptomcount={dashData.symptom_count} visitcount={dashData.response_count}/></Grid>
-                <Grid item>
-                    <CompletedSurveyCard>
-                        {completedSurveys.map((completed_surveys, index) => {
-                        return <Grid item><InfoCard key={index} title={completed_surveys.title} status={null} link={completed_surveys.id}/></Grid>
-                    })}
-                    </CompletedSurveyCard>
-                </Grid>
-
-                <Grid item>
-                    <TakeSurveyCard>
-                        {activeSurveys.map((active_surveys, index) => {
-                            return <Grid item key={index}><InfoCard title={active_surveys.title} link={active_surveys.id}/></Grid>
-                        })}
-                    </TakeSurveyCard>
-                </Grid>
-            </Grid>
-        </Grid>
-    </Grid>
-    </div>
-</Fade>
+                                <Grid item>
+                                    <TakeSurveyCard>
+                                        {activeSurveys.map((active_surveys, index) => {
+                                            return <Grid item key={index}><InfoCard title={active_surveys.title} link={active_surveys.id}/></Grid>
+                                        })}
+                                    </TakeSurveyCard>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </div>
+            </Fade>
+    );
 }
 export default DashboardPage;
