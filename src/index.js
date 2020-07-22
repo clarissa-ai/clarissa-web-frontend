5e3ddd0dfa3ff20ba466de44854cee78ae99dd89
ryanmao725 <ryan.mao.725@gmail.com>
2020-07-22 14:03:05 -0400

    Weee no longer display anything when drawer is gone

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './redux/store';
+import MobileBlock from 'components/navigation/mobileblock/MobileBlock';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <MobileBlock>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </MobileBlock>
     </React.StrictMode>,
     document.getElementById('root'),
 );
